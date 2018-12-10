a87df81757e1d9f80c48b63ab66d1faf4989c3b0
Kevin Emigh <kevin@waldoms.com>
2018-12-09 20:48:59 -0500

    moves binding adapters to the view model

--- app/src/main/java/com/waldoms/carddemo/CardViewModel.kt
+++ app/src/main/java/com/waldoms/carddemo/CardViewModel.kt
@@ -1,8 +1,25 @@
 package com.waldoms.carddemo
 
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.waldoms.carddemo.model.CardItem
 import com.waldoms.carddemo.model.CardItems
+
+@BindingAdapter("setCardAdapter")
+fun bindCardRecyclerAdapter(recyclerView: RecyclerView, adapter: CardRecyclerAdapter)
+{
+    recyclerView.layoutManager = LinearLayoutManager(recyclerView.context)
+    recyclerView.adapter = adapter
+}
+
+@BindingAdapter("setItemImage")
+fun bindItemImage(imageView: ImageView, imageId:Int)
+{
+    imageView.setImageResource(imageId)
+}
 
 class CardViewModel : ViewModel()
 {
--- app/src/main/java/com/waldoms/carddemo/CustomBinding.kt
+++ app/src/main/java/com/waldoms/carddemo/CustomBinding.kt
@@ -5,15 +5,3 @@ import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-@BindingAdapter("setCardAdapter")
-fun bindCardRecyclerAdapter(recyclerView: RecyclerView, adapter: CardRecyclerAdapter)
-{
-    recyclerView.layoutManager = LinearLayoutManager(recyclerView.context)
-    recyclerView.adapter = adapter
-}
-
-@BindingAdapter("setItemImage")
-fun bindItemImage(imageView:ImageView, imageId:Int)
-{
-    imageView.setImageResource(imageId)
-}
